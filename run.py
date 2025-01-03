87f088c90ee3ea4a69a59fd6dbc31c806df789a5
mihirb1 <109064732+mihirb1@users.noreply.github.com>
2025-01-03 00:16:18 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-#from flaskblog import app
+from flaskblog import app
 
-#if __name__ == '__main__':
-    #app.run(debug=True)
-
-if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True)
