1b372f1f97e586215d857b0bac37602456d0cfc5
mihirb1 <109064732+mihirb1@users.noreply.github.com>
2025-01-03 00:13:00 -0800

    run.py in heroku

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from flaskblog import app
+#from flaskblog import app
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
