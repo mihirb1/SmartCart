dff22d6684ba519e9e98a854e6b3652589b42095
mihirb1 <109064732+mihirb1@users.noreply.github.com>
2025-01-07 13:47:03 -0800

    no more posts in routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -16,8 +16,7 @@ def home():
    # uses URL link (ex. ?page=2)
    page = request.args.get('page', 1, type = int)
    # paginate website (newest posts first)
-   posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
-   return render_template('home.html', posts=posts)
+   return render_template('home.html')
 
 
 @app.route("/about")
